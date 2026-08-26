2c006bb3f809e5a946829a917a14febf42dd3933
ralaindev <ralaindev@gmail.com>
2026-08-26 12:48:29 +0200

    feat: add hexagonal structure and price JPA entity

---
+++ src/main/java/com/qindel/test/domain/model/Price.java
@@ -0,0 +1,5 @@
+package com.qindel.test.domain.model;
+
+public record Price() {
+    
+}
---
+++ src/main/java/com/qindel/test/infrastructure/output/database/entity/PriceJpaEntity.java
@@ -0,0 +1,43 @@
+package com.qindel.test.infrastructure.output.database.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "prices")
+public class PriceJpaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list", nullable = false)
+    private Long priceList;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer priority;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false, length = 3)
+    private String currency;
+}
